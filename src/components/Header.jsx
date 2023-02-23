96bdeef131a3a119a3185e46fbb452410e997d9f
Andri Purnomo <andri.adrp@gmail.com>
2023-02-23 10:08:13 +0700

    chore: initial setup material ui

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import AppBar from '@mui/material/AppBar';
+import ToolBar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+
+export default function Header() {
+  return (
+    <AppBar>
+      <ToolBar>
+        <Typography>Job Posting Application</Typography>
+      </ToolBar>
+    </AppBar>
+  );
+}
